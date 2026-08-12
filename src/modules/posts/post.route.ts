7f3134d90f8a0f561adbd4940071a92c326de385
Azharul Islam <azharulip@gmail.com>
2026-08-12 11:39:56 +0600

    get all posts

--- src/modules/posts/post.route.ts
+++ src/modules/posts/post.route.ts
@@ -4,5 +4,6 @@ import { postController } from "./post.controller";
 const router = express.Router();
 
 router.post("/create", postController.createPost);
+router.post("/all", postController.getAllPosts);
 
 export const postRouter: Router = router;
